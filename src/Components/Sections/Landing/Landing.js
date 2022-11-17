501106634aded114fe1110728c4fe2487e665caa
tonymichael00 <tonymichael00@gmail.com>
2022-11-17 16:24:47 -0600

    finished navbar.moving NavBar outside Landing.

--- src/Components/Sections/Landing/Landing.js
+++ src/Components/Sections/Landing/Landing.js
@@ -6,7 +6,15 @@ const Landing = () => {
   return (
     <div id="landing">
       <NavBar />
-      <p>Landing</p>
+      <div id="title">
+        <span className="shooz-txt yellow-txt">shooz</span>
+        <br /> with <span className="yellow-txt">Mike Schuster</span>
+      </div>
+      <div id="title-caption">
+        I can help solve your web problems.
+        <br /> Don't hesitate to
+        <span className="yellow-txt"> contact me.</span>
+      </div>
     </div>
   );
 };
--- src/Components/Sections/Landing/NavBar/NavBar.js
+++ src/Components/Sections/Landing/NavBar/NavBar.js
@@ -1,40 +1,47 @@
 import React from 'react';
 import './NavBar.css';
+import logo from './images/logo.png';
 
 const NavBar = () => {
   return (
-    <div id="nav-bar">
-      <nav>
-        <ul className="nav-links">
-          <li>
-            <a href="#About">
-              <span>About</span>
-            </a>
-          </li>
-          <li>
-            <a href="#Experience">
-              <span>Experience</span>
-            </a>
-          </li>
-          <li>
-            <a href="#Projects">
-              <span>Projects</span>
-            </a>
-          </li>
-          <li>
-            <a href="#Contact">
-              <span>Contact</span>
-            </a>
-          </li>
-          <li>
-            <a href="_self">
-              <span>Resume</span>
-            </a>
-          </li>
-        </ul>
+    <header>
+      <nav id="nav-bar">
+        <div id="logo" className="nav-links">
+          <a href="_self">
+            <img src={logo} alt="Logo" />
+          </a>
+        </div>
+        <div id="links">
+          <ul>
+            <li className="nav-links">
+              <a href="_self">
+                <span>About</span>
+              </a>
+            </li>
+            <li className="nav-links">
+              <a href="#Experience">
+                <span>Experience</span>
+              </a>
+            </li>
+            <li className="nav-links">
+              <a href="#Projects">
+                <span>Projects</span>
+              </a>
+            </li>
+            <li className="nav-links">
+              <a href="#Contact">
+                <span>Contact</span>
+              </a>
+            </li>
+            <li className="nav-links">
+              <a href="_self">
+                <span>Resume</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
-      NavBar
-    </div>
+    </header>
   );
 };
 
