3d13c861cc7907099840021c0ac8450020881c1a
tonymichael00 <tonymichael00@gmail.com>
2023-01-10 14:59:12 -0600

    fixed css and html to be more symmetric.

--- src/Components/Sections/Landing/Landing.js
+++ src/Components/Sections/Landing/Landing.js
@@ -9,18 +9,16 @@ const Landing = () => {
       <div id="title-page">
         <div id="title-bubble">
           <div className="shooz-bkd"></div>
-          <div id="title" className="center-flex">
+          <div id="title-txt" className="center-flex">
             <span className="shooz-txt yellow-txt">shooz</span>
           </div>
         </div>
         <div id="title-caption-bubble">
-          <div id="caption">
-            <div className="black-bkd right"></div>
-            <div id="caption-txt">
-              Hi! I'm <span className="yellow-txt">Mike Schuster.</span> I can
-              help solve your web problems. Don't hesitate to
-              <span className="yellow-txt"> contact me.</span>
-            </div>
+          <div className="caption-bkd"></div>
+          <div id="caption-txt">
+            Hi! I'm <span className="yellow-txt">Mike Schuster.</span> I can
+            help solve your web problems. Don't hesitate to
+            <span className="yellow-txt"> contact me.</span>
           </div>
         </div>
       </div>
