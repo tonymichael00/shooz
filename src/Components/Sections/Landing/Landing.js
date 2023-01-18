6b859f6528b7508f2f88305044bb0900a9faa24c
tonymichael00 <tonymichael00@gmail.com>
2023-01-18 10:39:15 -0600

    1/28/23

--- src/Components/Sections/Landing/Landing.js
+++ src/Components/Sections/Landing/Landing.js
@@ -8,13 +8,13 @@ const Landing = () => {
       <NavBar />
       <div id="title-sec">
         <div id="title-bubble">
-          <div className="shooz-bkd"></div>
+          <div id="shooz-bkd"></div>
           <div id="title-txt" className="center-flex">
             <span className="shooz-txt yellow-txt">shooz</span>
           </div>
         </div>
         <div id="title-caption-bubble">
-          <div className="caption-bkd"></div>
+          <div id="caption-bkd"></div>
           <div id="caption-txt">
             Hi! I'm <span className="yellow-txt">Mike Schuster.</span> I can
             help solve your web problems. Don't hesitate to
