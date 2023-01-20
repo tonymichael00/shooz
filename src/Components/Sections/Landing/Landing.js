8d8cd9d226e520cb42d44011cdfce3f27f457ebb
tonymichael00 <tonymichael00@gmail.com>
2023-01-20 12:28:06 -0600

    1/20/23

--- src/Components/Sections/Landing/Landing.js
+++ src/Components/Sections/Landing/Landing.js
@@ -10,15 +10,19 @@ const Landing = () => {
         <div id="title-bubble">
           <div id="shooz-bkd"></div>
           <div id="title-txt" className="center-flex">
-            <span className="shooz-txt yellow-txt">shooz</span>
+            <span id="shooz-txt" className="yellow-txt center-flex">
+              <p>shooz</p>
+            </span>
           </div>
         </div>
         <div id="title-caption-bubble">
           <div id="caption-bkd"></div>
-          <div id="caption-txt">
-            Hi! I'm <span className="yellow-txt">Mike Schuster.</span> I can
-            help solve your web problems. Don't hesitate to
-            <span className="yellow-txt"> contact me.</span>
+          <div id="caption-txt" className="center-flex">
+            <p>
+              Hi! I'm <span className="yellow-txt"> Mike Schuster.</span> I can
+              help solve your web problems. Don't hesitate to
+              <span className="yellow-txt"> contact me.</span>
+            </p>
           </div>
         </div>
       </div>
