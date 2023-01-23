35005c4ab3ee1bdd03eaa603e47f5b3eb510c466
tonymichael00 <tonymichael00@gmail.com>
2023-01-23 11:11:58 -0600

    1/23/23

--- src/Components/Sections/Landing/Landing.js
+++ src/Components/Sections/Landing/Landing.js
@@ -6,24 +6,22 @@ const Landing = () => {
   return (
     <div id="landing">
       <NavBar />
-      <div id="title-sec">
-        <div id="title-shape">
-          <div id="side-shooz-bkd"></div>
-          <div id="shooz-bkd" className="center-flex">
-            <span id="shooz-txt" className="yellow-txt center-flex">
-              <p>shooz</p>
-            </span>
-          </div>
+      <div id="title-shape">
+        <div id="side-shooz-bkd"></div>
+        <div id="shooz-bkd" className="center-flex">
+          <span id="shooz-txt" className="yellow-txt center-flex">
+            <p>shooz</p>
+          </span>
         </div>
-        <div id="title-caption-shape">
-          <div id="title-caption-side-bkd"></div>
-          <div id="title-caption-bkd" className="center-flex">
-            <p>
-              Hi! I'm <span className="yellow-txt"> Mike Schuster.</span> I can
-              help solve your web problems. Don't hesitate to
-              <span className="yellow-txt"> contact me.</span>
-            </p>
-          </div>
+      </div>
+      <div id="title-caption-shape">
+        <div id="title-caption-side-bkd"></div>
+        <div id="title-caption-bkd" className="center-flex">
+          <p>
+            Hi! I'm <span className="yellow-txt"> Mike Schuster.</span> I can
+            help solve your web problems. Don't hesitate to
+            <span className="yellow-txt"> contact me.</span>
+          </p>
         </div>
       </div>
     </div>
