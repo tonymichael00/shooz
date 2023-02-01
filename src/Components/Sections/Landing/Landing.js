b63e63074b4907410860a3cd8add6962f4ab7d6c
tonymichael00 <tonymichael00@gmail.com>
2023-02-01 17:53:15 -0600

    redid front page text. Fixed width problems. Cleaned CSS.

--- src/Components/Sections/Landing/Landing.js
+++ src/Components/Sections/Landing/Landing.js
@@ -9,18 +9,25 @@ const Landing = () => {
       <div id="shooz-bkd-wrapper">
         <div id="side-shooz-bkd"></div>
         <div id="shooz-bkd">
-          <span id="shooz-txt" className="yellow-txt center-flex">
-            <p className="center-flex">shooz</p>
-          </span>
+          <p className="center-flex">
+            <span className="shooz-title yellow-txt center-flex">shooz</span>
+          </p>
+          <p className="shooz-subtitle center-flex">
+            <span className="shooz-subtitle white-txt">with </span>
+            &nbsp;
+            <span className="shooz-subtitle yellow-txt"> Mike Schuster</span>
+          </p>
         </div>
       </div>
       <div id="title-caption-wrapper">
         <div id="title-caption-side-bkd"></div>
         <div id="title-caption-bkd" className="center-flex">
           <p>
-            Hi! I'm <span className="yellow-txt"> Mike Schuster.</span> I can
-            help solve your web problems.
-            <br /> Don't hesitate to
+            I can help <span className="yellow-txt">solve </span>your web
+            problems.
+          </p>
+          <p>
+            Don't hesitate to
             <span className="yellow-txt"> contact me.</span>
           </p>
         </div>
