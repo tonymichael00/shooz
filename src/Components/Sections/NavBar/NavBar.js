ef5876fdc1b3fbbf801631ce0ea36c190afc5114
tonymichael00 <tonymichael00@gmail.com>
2023-02-17 18:15:43 -0600

    organized css. working on hamburger menu.

--- src/Components/Sections/NavBar/NavBar.js
+++ src/Components/Sections/NavBar/NavBar.js
@@ -15,14 +15,6 @@ const NavBar = () => {
         </a>
         {/* div to create space in grid */}
         <div id="ham-menu">
-          {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              height="2.6rem"
-              width="3rem"
-            >
-              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-            </svg> */}
           <HamMenu />
         </div>
         <div className="nav-links-cont-click">
