43a94dae78a3a5438a6f666229836d19d7faa9ca
tonymichael00 <tonymichael00@gmail.com>
2023-04-07 14:38:55 -0500

    fixed colors for swiper

--- src/Components/Projects/Carousel/Carousel.js
+++ src/Components/Projects/Carousel/Carousel.js
@@ -1,3 +1,6 @@
+/*TODO: CROP IMAGES TO SAME ASPECT RATIO
+ */
+
 import React, { useEffect, useRef } from 'react';
 import './Carousel.scss';
 import { register } from 'swiper/element/bundle';
@@ -5,6 +8,7 @@ import img1 from './Images/img1.jpg';
 import img2 from './Images/img2.jpg';
 import img3 from './Images/img3.jpg';
 import img4 from './Images/img4.jpg';
+
 register();
 
 const Carousel = () => {
@@ -29,21 +33,24 @@ const Carousel = () => {
       slides-per-view="1"
       navigation="true"
       pagination="true"
+      // loop="true"
+      fill="column"
+      // scrollbar="true"
     >
       <swiper-slide>
         <img src={img1} alt="Logo" width={'100%'} height={'100%'} />
       </swiper-slide>
       <swiper-slide>
         {' '}
-        <img src={img2} alt="Logo" width={'100%'} />
+        <img src={img2} alt="Logo" width={'100%'} height={'100%'} />
       </swiper-slide>
       <swiper-slide>
         {' '}
-        <img src={img3} alt="Logo" width={'100%'} />
+        <img src={img3} alt="Logo" width={'100%'} height={'100%'} />
       </swiper-slide>
       <swiper-slide>
         {' '}
-        <img src={img4} alt="Logo" width={'100%'} />
+        <img src={img4} alt="Logo" width={'100%'} height={'100%'} />
       </swiper-slide>
       ...
     </swiper-container>
